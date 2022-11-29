d0688bf489063e5418f5cb86c46d531c1b3dc8a8
Sergey Deev <mygrayroom@gmail.com>
2022-11-29 08:54:56 +0600

    Init

---
+++ src/com/jdbc/example/util/ConnectionManager.java
@@ -0,0 +1,24 @@
+package com.jdbc.example.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class ConnectionManager {
+    public static final String URL = "db.example.url";
+    public static final String USER = "db.example.user";
+    public static final String PASSWORD = "db.example.password";
+
+    private ConnectionManager() {
+    }
+
+    public static Connection openConnection() {
+        try {
+            return DriverManager.getConnection(PropertiesUtil.get(URL),
+                    PropertiesUtil.get(USER),
+                    PropertiesUtil.get(PASSWORD));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
