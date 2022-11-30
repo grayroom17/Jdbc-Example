b04bd5ed98889d5a6e6fb4d86d6417cb10b10afb
Sergey Deev <mygrayroom@gmail.com>
2022-11-30 09:20:48 +0600

    add Statement execute method examples with DDL commands

--- src/com/jdbc/example/JdbcExample.java
+++ src/com/jdbc/example/JdbcExample.java
@@ -1,13 +1,12 @@
 package com.jdbc.example;
 
+import com.jdbc.example.statement.execute.StatementExecuteDdlExamples;
 import com.jdbc.example.util.ConnectionManager;
 
 import java.sql.SQLException;
 
 public class JdbcExample {
     public static void main(String[] args) throws SQLException {
-        try (var connection = ConnectionManager.openConnection()) {
-            System.out.println(connection.getTransactionIsolation());
-        }
+        StatementExecuteDdlExamples.createTable();
     }
 }
---
+++ src/com/jdbc/example/statement/execute/StatementExecuteDdlExamples.java
@@ -0,0 +1,44 @@
+package com.jdbc.example.statement.execute;
+
+import com.jdbc.example.util.ConnectionManager;
+
+import java.sql.SQLException;
+
+public class StatementExecuteDdlExamples {
+    public static void createTable() throws SQLException {
+        String sql = """
+                     create table if not exists employee(
+                     id serial primary key,
+                     first_name varchar,
+                     last_name varchar,
+                     birthdate date)
+                     """;
+        try (var connection = ConnectionManager.openConnection();
+             var statement = connection.createStatement()) {
+            var result = statement.execute(sql);
+            System.out.println(result);
+        }
+    }
+
+    public static void createDatabase() throws SQLException {
+        var sql = """
+                  create database example_database;
+                  """;
+        try (var connection = ConnectionManager.openConnection();
+             var statement = connection.createStatement()) {
+            var result = statement.execute(sql);
+            System.out.println(result);
+        }
+    }
+
+    public static void createSchema() throws SQLException {
+        var sql = """
+                  create schema if not exists example_schema;
+                  """;
+        try (var connection = ConnectionManager.openConnection();
+             var statement = connection.createStatement()) {
+            var result = statement.execute(sql);
+            System.out.println(result);
+        }
+    }
+}
