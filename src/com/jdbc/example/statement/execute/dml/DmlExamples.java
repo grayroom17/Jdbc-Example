e878258c3f919128bcf9115befd7dbf09501151a
Sergey Deev <mygrayroom@gmail.com>
2022-12-01 09:11:21 +0600

    add SELECT examples

--- src/com/jdbc/example/statement/execute/dml/DmlExamples.java
+++ src/com/jdbc/example/statement/execute/dml/DmlExamples.java
@@ -13,15 +13,15 @@ public class DmlExamples {
 
     public static void insertWithExecute() throws SQLException {
         var sql = """
-                  insert into employee(first_name,last_name,birthdate)
+                  insert into employee(first_name,last_name,birthdate,salary,department_id)
                   values
-                  ('Ivanov', 'Ivan', '1983-10-02'),
-                  ('Petrov', 'Petr', '1975-05-10');
-                  insert into employee(first_name,last_name,birthdate)
+                  ('Ivanov', 'Ivan', '1983-10-02',150000,2),
+                  ('Petrov', 'Petr', '1975-05-10',100000,3);
+                  insert into employee(first_name,last_name,birthdate,salary,department_id)
                   values
-                  ('Vlasov', 'Stepan', '193-11-17'),
-                  ('Pupkin', 'Vladimir', '1998-12-01'),
-                  ('Dudkin', 'Sergey', '1993-03-08');
+                  ('Vlasov', 'Stepan', '193-11-17',90000,4),
+                  ('Pupkin', 'Vladimir', '1998-12-01',120000,5),
+                  ('Dudkin', 'Sergey', '1993-03-08',45000,6);
                   """;
         try (var connection = ConnectionManager.openConnection();
              var statement = connection.createStatement()) {
