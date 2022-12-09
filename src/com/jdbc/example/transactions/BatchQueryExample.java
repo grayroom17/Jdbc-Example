7336dfdb6e91add85aef2e50afcf258834c3e005
Sergey Deev <mygrayroom@gmail.com>
2022-12-09 09:28:50 +0600

    add batch example

---
+++ src/com/jdbc/example/transactions/BatchQueryExample.java
@@ -0,0 +1,60 @@
+package com.jdbc.example.transactions;
+
+import com.jdbc.example.util.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BatchQueryExample {
+    public static void main(String[] args) throws SQLException {
+        tryBatchQuery();
+    }
+
+    private static void tryBatchQuery() throws SQLException {
+        var addEmployees = """
+                           insert into employee(last_name,first_name,birthdate,salary,department_id)
+                           values
+                           ('Ivanov', 'Ivan', '1995-04-27',999999,2),
+                           ('Petrushkin', 'Boris', '1996-03-30',777777,2);
+                           """;
+        var deleteEmployee = """
+                                 delete from employee
+                                 where last_name = %s
+                                 and first_name = %s
+                                 and birthdate = %s
+                                 and department_id = %d
+                             """.formatted("'Ivanov'", "'Ivan'", "'1995-04-27'", 2);
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            connection = ConnectionManager.openConnection();
+            connection.setAutoCommit(false);
+
+            statement = connection.createStatement();
+
+            statement.addBatch(addEmployees);
+            statement.addBatch(deleteEmployee);
+
+            var result = statement.executeBatch();
+
+            for (int j : result) {
+                System.out.println(j);
+            }
+
+            connection.commit();
+        } catch (Exception e) {
+            if (connection != null) {
+                connection.rollback();
+                throw e;
+            }
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        if (statement != null) {
+            statement.close();
+        }
+    }
+}
