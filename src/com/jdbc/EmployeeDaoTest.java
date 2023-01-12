cbf31caf4142eddea501a7373ce99e76b348c353
Sergey Deev <mygrayroom@gmail.com>
2023-01-12 09:49:29 +0600

    add Slf4j logger

--- src/com/jdbc/EmployeeDaoTest.java
+++ src/com/jdbc/EmployeeDaoTest.java
@@ -3,10 +3,14 @@ package com.jdbc;
 import com.jdbc.dao.EmployeeDao;
 import com.jdbc.entity.Employee;
 import com.jdbc.filter.EmployeeFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 
 public class EmployeeDaoTest {
+    private static final Logger log = LoggerFactory.getLogger(EmployeeDaoTest.class);
+
     public static void main(String[] args) {
         var employeeDao = EmployeeDao.getInstance();
         var employee = new Employee("Ivan",
@@ -15,25 +19,26 @@ public class EmployeeDaoTest {
                 null,
                 999999L);
         var savedEmployee = employeeDao.save(employee);
-        System.out.println(savedEmployee);
+        log.info("{}", savedEmployee);
 
         savedEmployee.setSalary(300L);
         employeeDao.updateById(savedEmployee);
 
         var updatedEmployee = employeeDao.findById(savedEmployee.getId());
-        System.out.println(updatedEmployee);
+        log.info("{}", updatedEmployee);
 
         employeeDao.deleteById(savedEmployee.getId());
 
         var employees = employeeDao.findAll();
-        System.out.println(employees);
+        log.info("{}", employees);
 
-        var employeeFilter = new EmployeeFilter(3, 0, null, null, LocalDate.of(1799, 05, 26), null, null);
+        var employeeFilter = new EmployeeFilter(3, 0, null, null, LocalDate.of(1799, 5, 26), null, null);
         var filteredEmployees = employeeDao.findAll(employeeFilter);
-        System.out.println(filteredEmployees);
+        log.info("{}", filteredEmployees);
 
         var employeeFilter1 = new EmployeeFilter(3, 3, null, null, null, null, null);
         var filteredEmployees1 = employeeDao.findAll(employeeFilter1);
-        System.out.println(filteredEmployees1);
+        log.info("{}", filteredEmployees1);
+
     }
 }
